54042184011b3a3794c326ea898780396dac2e42
yunlongxiao <yunlongxiao@anjuke.com>
2012-12-24 13:03:07 +0800

    配置文件模板

--- src/d.corp.anjuke.com
+++ src/d.corp.anjuke.com
@@ -1,4 +1,3 @@
 $ORIGIN d.corp.anjuke.com
-testtest    A    192.168.200.84
-@    A    192.168.220.84
+e    A    127.0.0.1
 test    A    192.168.200.84
--- src/sql2config.php
+++ src/sql2config.php
@@ -2,12 +2,15 @@
     require_once("select.php");
     function writeConfig()
     {
-        $fp = fopen("d.corp.anjuke.com","w");
+        $template = 'd.corp.anjuke.com.template';
+        $file = 'd.corp.anjuke.com';
+        if(!copy($template,$file)) {
+            echo "Failed to copy $template.\n";
+        }
+        $fp = fopen("$file","a+");
         if($fp)
         {
-            $first_line = "\$ORIGIN d.corp.anjuke.com\n";
             global $userinfo;
-            fwrite($fp,$first_line);
             $result = getAll($userinfo['username']);
             while($row = mysql_fetch_array($result))
             {
