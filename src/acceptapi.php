8035482a0efc1bf3f5a1f8b65188c8154c71d5b7
yunlongxiao <yunlongxiao@anjuke.com>
2012-12-29 10:34:53 +0800

    fix bug

--- src/acceptapi.php
+++ src/acceptapi.php
@@ -2,12 +2,14 @@
 require("check.php");
 require("editrecord.php");
 require("key.php");
+require("sql2config.php");
 require("conn.php");
 
 if(!empty($_GET['dnsname']) && !empty($_GET['key']) && !empty($_GET['username'])) {
         $key = getKey($_GET['username']);
         if(isName($_GET['dnsname']) || $key==$_GET['key']) {
             addRecord($_GET['dnsname'],$_SERVER['REMOTE_ADDR'],$_GET['username']);
+            bindConfig();
         }
 }
 ?>
